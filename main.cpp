ad1af6f7d491379754326858faadc2618c5ce939
DuParkLinux <dupark3@gmail.com>
2018-04-23 15:02:46 -0400

    Infect Border function implemented. Other parts outlined

--- main.cpp
+++ main.cpp
@@ -1,12 +1,119 @@
+/*
+Leet Code #130
+
+1. Surrounded regions shouldn’t be on the border, which means that any 'O' on the
+   border of the board are not flipped to 'X'. 
+2. Any 'O' that is not on the border and it is not connected to an 'O' on the
+   border will be flipped to 'X'. 
+3. Two cells are connected if they are adjacent cells connected horizontally or
+   vertically.  
+
+*/
+#include <cstddef> // size_t
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
 class Solution {
 public:
-    void solve(vector<vector<char>>& board) {
+    bool InfectNeighbors(vector< vector<char> >& board){
+        size_t max_rows = board.size();
+        size_t max_columns = board[0].size();
+        bool InfectionMade = false;
+
+        return InfectionMade;
+    }
+
+    void InfectBorders(vector< vector<char> >& board){
+        size_t max_rows = board.size();
+        size_t max_columns = board[0].size();
+
+        for (int row = 0; row != max_rows; ++row){
+
+            for (int column = 0; column != max_columns; ++column){
+                if (row == 0) { // on top row
+                    if(board[row][column] == 'O') {
+                        board[row][column] = 'I';
+                        if (board[row+1][column] == 'O') board[row+1][column] = 'I';
+                    }
+                } else if (row != 0 && row != max_rows - 1) { // in middle rows
+                    if (column == 0 || column == max_columns -1){ // on left/right edge
+                        if (board[row][column] == 'O') {
+                            board[row][column] = 'I'; 
+                            if (board[row-1][column] == 'O') board[row-1][column] = 'I';
+                            if (board[row+1][column] == 'O') board[row+1][column] = 'I';
+                            if (column == 0 && board[row][column+1] == 'O') board[row][column+1] = 'I';
+                            if (column == max_columns-1 && board[row][column-1] == 'O') board[row][column-1] = 'I';
+                        }
+                    }
+                    if (column == 0){
+                        column = max_columns - 2; // will advance next loop to max_columns-1, the last col
+                    }
+                } else if (row == max_rows - 1) { // on bottom row
+                    if(board[row][column] == 'O') {
+                        board[row][column] = 'I';
+                        if (board[row-1][column] == 'O'){
+                            board[row-1][column] = 'I';
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+
+    void FlipRemainingOs(vector< vector<char> >& board){
+
+
+    }
+
+    void Disinfect(vector< vector<char> >& board){
         
+    }
+
+    void solve(vector< vector<char> >& board) {
+        size_t max_rows = board.size();
+        size_t max_columns = board[0].size();
+        
+        // initial infect: go through and "infect" border O's into I's 
+        InfectBorders(board);
+        
+        // continuous infect: go through and "infect" until no more infections happen
+        while(InfectNeighbors(board))
+            ; // nothing to do but keep infecting
+
+        // flip all remaining uninfected O's into X's
+        FlipRemainingOs(board);
+
+        // flip back infected I's into O's
+        Disinfect(board);
     }
 };
 
-int main(){
+/*if (board[row][column] == 'I'){
+    if (board[row+1][column] == 'O') board[row+1][column]='I';
+    if (board[row-1][column] == 'O') board[row-1][column]='I';
+    if (column != max_columns - 1 && board[row][column+1] == 'O') board[row][column+1]='I';
+    if (column != 0 && board[row][column-1] == 'O') board[row][column-1]='I';
 
-    
+}*/
+
+int main(){
+    vector< vector<char> > board= { {'X', 'X', 'O', 'X'},
+                                    {'X', 'O', 'O', 'X'},
+                                    {'X', 'X', 'O', 'X'},
+                                    {'X', 'O', 'X', 'X'}};
+
+    Solution s;
+    s.solve(board);
+
+    for (int row = 0; row != board.size(); ++row){
+        for (int column = 0; column != board[0].size(); ++column){
+            cout << board[row][column];
+        }
+    cout << endl;
+    }
+
     return 0;
 }
